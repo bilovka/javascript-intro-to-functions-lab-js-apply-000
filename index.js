da36a610d5aa60b05c7b686121f3708b24079029
Oleksandr <bilovka10@gmail.com>
2020-08-05 19:16:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ function sayHiToGrandma(string){
   var uppercase = "HELLO!"
   var lowercase = "hello"
   var mixedcase = "Hi there"
-
+lowercase.toLowerCase === string
+return "I can't hear you!"
 
 }
