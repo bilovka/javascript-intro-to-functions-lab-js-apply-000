680ee3f0f3a9f5306c1f415f8c235ea8f8a26b80
Oleksandr <bilovka10@gmail.com>
2020-08-05 19:08:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
   //string.toUpperCase()===uppercase
   //return "YES INDEED!"
   
-  string.toLowerCase()===lowercase
+  if(string.toLowerCase()===lowercase)
   return "I can't hear you"
   
   string.toLowerCase()===mixedcase
